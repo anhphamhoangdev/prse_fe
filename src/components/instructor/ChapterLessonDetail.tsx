1abab33949a01eb5002e65b91b70cbea31b3fb56
Anh Pham Hoang <harrispham.dev@gmail.com>
2025-06-02 11:47:19 +0700

    update moderate

--- src/components/instructor/ChapterLessonDetail.tsx
+++ src/components/instructor/ChapterLessonDetail.tsx
@@ -324,7 +324,7 @@ const ChapterLessons: React.FC<ChapterLessonsProps> = ({
                                 className="flex items-center gap-2 px-4 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700"
                             >
                                 <Plus className="w-4 h-4"/>
-                                Thêm bài học
+                                Thêm Bài Học
                             </button>
                         </>
                     ) : (
